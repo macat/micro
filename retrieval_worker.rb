ffe060de0599149450535f4c06066047ed7750d9
Rafael George <george.rafael@gmail.com>
2014-10-08 10:37:23 -0400

    Remove some unneeded code from RetrievalWorker

--- retrieval_worker.rb
+++ retrieval_worker.rb
@@ -1,11 +1,11 @@
-require 'json'
+require_relative 'lib/micro'
 
 class RetrievalWorker
   include Shoryuken::Worker
 
   def perform(sqs_msg)
-    outbound_message = GeoLocator.(sqs_msg)
-    OutboundMessageSender.(outbound_message)
+    outbound_message = Micro::GeoLocator.(sqs_msg.body)
+    Micro::OutboundMessageSender.('default', outbound_message)
   end
 end
 
