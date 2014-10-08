1d393aca58acf3a210485b8360a677aa8b0f0561
Rafael George <george.rafael@gmail.com>
2014-10-08 19:52:12 -0400

    Add proper handling of parsing errors for json messages

--- lib/micro/messages/inbound.rb
+++ lib/micro/messages/inbound.rb
@@ -7,16 +7,24 @@ module Micro
 
       attr_reader :page, :ip, :time, :message
       def initialize(msg)
+        @valid   = true
         @message = parse(msg)
         @page    = @message['page']
         @ip      = @message['ip']
         @time    = @message['time']
       end
 
+      def valid?
+        @valid
+      end
+
       private 
 
       def parse(msg)
         JSON.parse(msg)
+      rescue JSON::ParserError
+        @valid = false
+        {}
       end
     end
   end
