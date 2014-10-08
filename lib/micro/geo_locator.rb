d7811560e9cd597c3cedf4df4d91f81771c41101
Rafael George <george.rafael@gmail.com>
2014-10-08 10:16:08 -0400

    Add Micro::GeoLocator::GeocoderData value object and move the default attributes logic from Messages::Outbound

--- lib/micro/geo_locator.rb
+++ lib/micro/geo_locator.rb
@@ -6,8 +6,29 @@ module Micro
 
     def call(msg)
       inbound_message = Messages::Inbound.for(msg)
-      geocoder_data = Geocoder.search(inbound_message.ip).first.data
+      geocoder_data = GeocoderData.for(Geocoder.search(inbound_message.ip).first.data)
       outbound_message = Messages::Outbound.for(inbound_message, geocoder_data)
+    end
+
+    class GeocoderData 
+      def self.for(data) 
+        new(data) 
+      end
+
+      attr_reader :lat, :long
+      def initialize(data) 
+        @lat = default_attributes.merge(data).fetch('latitude')
+        @long = default_attributes.merge(data).fetch('longitude')
+      end
+
+      private 
+
+      def default_attributes 
+        {
+          'latitude' => 0,
+          'longitude' => 0
+        }
+      end
     end
   end
 end
