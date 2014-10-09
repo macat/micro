232659ec3514b8a929efc55da2d955bd763c00e4
Rafael George <george.rafael@gmail.com>
2014-10-08 20:03:33 -0400

    Add proper inbound invalid message handling for Micro::GeoLocator service

--- lib/micro/geo_locator.rb
+++ lib/micro/geo_locator.rb
@@ -4,10 +4,16 @@ module Micro
   module GeoLocator
     extend self
 
+    class InvalidInboundMessage < StandardError; end
+
     def call(msg)
       inbound_message = Messages::Inbound.for(msg)
-      geocoder_data = GeocoderData.for(Geocoder.search(inbound_message.ip).first.data)
-      outbound_message = Messages::Outbound.for(inbound_message, geocoder_data)
+      if inbound_message.valid?
+        geocoder_data = GeocoderData.for(Geocoder.search(inbound_message.ip).first.data)
+        outbound_message = Messages::Outbound.for(inbound_message, geocoder_data)
+      else
+        raise InvalidInboundMessage.new("The inbound message is invalid")
+      end
     end
 
     class GeocoderData 
--- spec/integration/geo_locator_spec.rb
+++ spec/integration/geo_locator_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper' 
 
 describe Micro::GeoLocator do 
-  let(:inbound_message) do 
-    '{"page": "/users", "time": 1411615161, "ip": "12.130.117.132"}'
+  context 'when valid inbound message' do 
+    let(:inbound_message) do 
+      '{"page": "/users", "time": 1411615161, "ip": "12.130.117.132"}'
+    end
+
+    it 'retrieve the longitude and latitude for a given ip address' do 
+      expect(Micro::GeoLocator.(inbound_message).lat).to eql 38
+      expect(Micro::GeoLocator.(inbound_message).long).to eql -97
+    end
   end
 
-  it 'retrieve the longitude and latitude for a given ip address' do 
-
-    expect(Micro::GeoLocator.(inbound_message).lat).to eql 38
-    expect(Micro::GeoLocator.(inbound_message).long).to eql -97
+  context 'when invalid inbound message' do 
+    let(:inbound_message) { '{page: "/users"}' }
+    it 'raises an error' do 
+      expect { 
+        Micro::GeoLocator.(inbound_message)
+      }.to raise_error(Micro::GeoLocator::InvalidInboundMessage, "The inbound message is invalid")
+    end
   end
 end
