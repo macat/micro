da5a8c2c906457cbd8f74c59afe7d3c4cd3f1516
Rafael George <george.rafael@gmail.com>
2014-10-08 21:41:54 -0400

    Add Micro::OutbountMessageSaver service to save into the cache

---
+++ lib/micro/outbound_message_saver.rb
@@ -0,0 +1,20 @@
+require 'redis'
+
+module Micro 
+  module OutboundMessageSaver 
+    extend self 
+    EXPIRATION_TIME = 86400
+
+    def call(message)
+      key_name = "msg-#{message.ip}"
+      redis.set(key_name, message.to_json)
+      redis.expire(key_name, EXPIRATION_TIME)
+    end
+
+    private 
+
+    def redis(options = {})
+      Redis.new(options)
+    end
+  end
+end
